f9d334b78867193da1c1c7839a782d5ec362596b
Joe Reid <jreid@vnet.net>
2014-08-09 21:21:19 -0500

    cleanup

--- FoodWuzUp.DAL/Group/Group.cs
+++ FoodWuzUp.DAL/Group/Group.cs
@@ -15,21 +15,20 @@ namespace FoodWuzUp.DAL
 
         public int? CreatorID { get; set; }
 
-
         public virtual User Creator { get; set; }
-       public virtual ICollection<GroupUser> Members { get; set; }
-       public virtual ICollection<Restaurant> Restaurants { get; set; }
-       public virtual ICollection<GroupComment> Comments { get; set; }
-       public virtual ICollection<Employee> Employees { get; set; }
+        public virtual ICollection<GroupUser> Members { get; set; }
+        public virtual ICollection<Restaurant> Restaurants { get; set; }
+        public virtual ICollection<GroupComment> Comments { get; set; }
+        public virtual ICollection<Employee> Employees { get; set; }
 
 
         public Group()
-       {
-           Comments = new List<GroupComment>();
-           Members = new List<GroupUser>();
-           Restaurants = new List<Restaurant>();
-           Employees = new List<Employee>();
-       }
+        {
+            Comments = new List<GroupComment>();
+            Members = new List<GroupUser>();
+            Restaurants = new List<Restaurant>();
+            Employees = new List<Employee>();
+        }
 
     }
 }
