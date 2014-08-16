5cd4bf0fe9b5ad40c3cef68a9bbce53fc96cd328
nathanjuett <nathanjuett5@gmail.com>
2014-08-15 21:59:22 -0500

    Added Events and EventTypes

---
+++ FoodWuzUp.DAL.Test/EventTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FoodWuzUp.DAL;
+using System.Linq;
+
+namespace FoodWuzUp.DAL.Test
+{
+    [TestClass]
+    public class EventTests
+    {
+        [TestMethod]
+        public void TestCreate()
+        {
+            Context db = new Context(new DemoContextInitializer());
+            db.Events.Add(new Event() { UserID = 1, RequestingUserID = 1, EventTypeID = 1, Name = "test" });
+            db.SaveChanges();
+        }
+    }
+}
---
+++ FoodWuzUp.DAL/Event/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodWuzUp.DAL
+{
+    public class Event : Base<Event>
+    {
+        public int UserID { get; set; }
+        public int RequestingUserID { get; set; }
+        public int EventTypeID { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public string Message { get; set; }
+        public virtual EventType EventType { get; set; }
+        public virtual User User { get; set; }
+
+    }
+}
